2425be4a9f021d85879a11e66773c33608eeb235
paulpog <paulpog@japarasolutions.com>
2024-01-05 15:27:12 +0000

    define github actions AWS access role

--- deployment/test/main.tf
+++ deployment/test/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     region         = "eu-west-2"
-    bucket         = "pp-terraform-status"
+    bucket         = "japara-terraform-state"
     key            = "platform/test-terraform.tfstate"
     dynamodb_table = "platform-terraform-locks"
     encrypt        = true
